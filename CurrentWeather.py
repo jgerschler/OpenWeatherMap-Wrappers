cc8cd520b2e69b22fb241c97a6b5eaea80aab590
jgerschler <jjgerschler@gmail.com>
2017-01-18 17:52:03 -0600

    in progress

--- CurrentWeather.py
+++ CurrentWeather.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from urllib2 import Request, urlopen, URLError
 
 class CurrentWeather(object):
+    """A class for getting the current weather using the OpenWeatherMap API."""
     def __init__(self):
         pass
 
@@ -21,9 +22,33 @@ class CurrentWeather(object):
         self.country = self.decoded_string.get('sys', 'unknown').get('country', 'unknown')
         self.sunset_time = self.decoded_string.get('sys', 'unknown').get('sunset', 'unknown')
         self.sunrise_time = self.decoded_string.get('sys', 'unknown').get('sunrise', 'unknown')
-        self.weather_cond_id = self.decoded_string.get('weather', 'unknown').get('id', 'unknown')
-        self.weather_group = self.decoded_string.get('weather', 'unknown').get('main', 'unknown')
-        self.weather_description = self.decoded_string.get('weather', 'unknown').get('description', 'unknown')
+        self.weather_cond_id = self.decoded_string.get('weather', 'unknown')[0].get('id', 'unknown')# multiple weather conditions can be included here. 
+        self.weather_group = self.decoded_string.get('weather', 'unknown')[0].get('main', 'unknown')# currently, we'll just get the primary.
+        self.weather_description = self.decoded_string.get('weather', 'unknown')[0].get('description', 'unknown')
+        self.rain_3h = self.decoded_string.get('rain', 'unknown').get('3h', 'unknown')
+        self.pressure = self.decoded_string.get('main', 'unknown').get('pressure', 'unknown')
+        self.temp_min = self.decoded_string.get('main', 'unknown').get('temp_min', 'unknown')
+        self.temp_max = self.decoded_string.get('main', 'unknown').get('temp_max', 'unknown')
+        self.temp = self.decoded_string.get('main', 'unknown').get('temp', 'unknown')
+        self.humidity = self.decoded_string.get('main', 'unknown').get('humidity', 'unknown')
+        self.city_id = self.decoded_string.get('id', 'unknown')
+        self.wind_speed = self.decoded_string.get('wind', 'unknown').get('deg', 'unknown')
+        self.wind_gust = self.decoded_string.get('wind', 'unknown').get('gust', 'unknown')
+        self.wind_direction = self.decoded_string.get('wind', 'unknown').get('deg', 'unknown')
+        
+    def parser(self, data):
+        self.decoded_string = json.loads(data)
+        self.data_collection_time = self.decoded_string.get('dt', 'unknown')
+        self.cloud_cover = self.decoded_string.get('clouds', 'unknown').get('all', 'unknown')
+        self.city_name = self.decoded_string.get('name', 'unknown')
+        self.longitude = self.decoded_string.get('coord', 'unknown').get('lon', 'unknown')
+        self.latitude = self.decoded_string.get('coord', 'unknown').get('lat', 'unknown')
+        self.country = self.decoded_string.get('sys', 'unknown').get('country', 'unknown')
+        self.sunset_time = self.decoded_string.get('sys', 'unknown').get('sunset', 'unknown')
+        self.sunrise_time = self.decoded_string.get('sys', 'unknown').get('sunrise', 'unknown')
+        self.weather_cond_id = self.decoded_string.get('weather', 'unknown')[0].get('id', 'unknown')
+        self.weather_group = self.decoded_string.get('weather', 'unknown')[0].get('main', 'unknown')
+        self.weather_description = self.decoded_string.get('weather', 'unknown')[0].get('description', 'unknown')
         self.rain_3h = self.decoded_string.get('rain', 'unknown').get('3h', 'unknown')
         self.pressure = self.decoded_string.get('main', 'unknown').get('pressure', 'unknown')
         self.temp_min = self.decoded_string.get('main', 'unknown').get('temp_min', 'unknown')
@@ -98,7 +123,7 @@ class CurrentWeather(object):
                 return 'S'
             elif degrees <= 247.5 and degrees > 202.5:
                 return 'SW'                
-             elif degrees <= 292.5 and degrees > 247.5:
+            elif degrees <= 292.5 and degrees > 247.5:
                 return 'W'
             elif degrees <= 337.5 and degrees > 292.5:
                 return 'NW'
